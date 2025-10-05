49c2242c1f1a55ba66a78f23c73a732f5a893886
uni <gugurutan@demons-door.sakura.ne.jp>
2025-10-05 16:00:49 +0900

    コマンド追加

--- src/Providers/LaravelDevSupportServiceProvider.php
+++ src/Providers/LaravelDevSupportServiceProvider.php
@@ -4,6 +4,7 @@ namespace LaravelDevSupport\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Artisan;
+use LaravelDevSupport\Console\Commands\TestCommand;
 
 class LaravelDevSupportServiceProvider extends ServiceProvider
 {
@@ -13,12 +14,25 @@ class LaravelDevSupportServiceProvider extends ServiceProvider
         // $this->loadRoutesFrom(__DIR__, '/../routes/console.php');
         // views
         // $this->loadViewsFrom(__DIR__ . '/../resources/views', 'laravel-dev-support');
+
+        // commands
+        $this->app->singleton(TestCommand::class);
+
+        $this->commands([
+            TestCommand::class,
+        ]);
     }
 
     public function register()
     {
         // commands
         // Artisan::registerCommand(resolve(\LaravelDevSupport\Console\Commands\TestCommand::class));
+        // ↑を書くと下記エラーになる。
+        // > Illuminate\Foundation\ComposerScripts::postAutoloadDump
+        // > @php artisan package:discover --ansi
+        //    ERROR  There are no commands defined in the "package" namespace.  
+        // Script @php artisan package:discover --ansi handling the post-autoload-dump event returned with error code 1
+
         // services
     }
 }
